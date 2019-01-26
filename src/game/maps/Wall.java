b2140b63a64e8f053dcaea34a54e889d4c36b470
Ho Xuan Hoang Duong <hxhduong4394@gmail.com>
2019-01-27 04:54:01 +0700

    Saving files before refreshing line endings

--- src/game/maps/Wall.java
+++ src/game/maps/Wall.java
@@ -12,14 +12,19 @@ public class Wall extends GameObject {
 
     public int isType;
 
-    public Wall(String url) {
+    public Wall() {
         super();
         if(url != null){
             BufferedImage image = SpriteUtils.loadImage(url);
             this.renderer = new SingleImageRenderer(image);
         };
-        this.boxColider = new BoxColider(this.position,20,20);
+        this.boxColider = new BoxColider(this,20,20);
 
+    }
+
+    public Wall(int width,int height) {
+        super();
+        this.boxColider = new BoxColider(this,width, height);
     }
 
     public void addImage(int dataImage) {
@@ -38,58 +43,79 @@ public class Wall extends GameObject {
                 break;
             }
 
-            case 1: {
-                isType = 1;
-                BufferedImage image = SpriteUtils.loadImage("wall_colum.tsx");
+            case 3: {
+                isType = 3;
+                BufferedImage image = SpriteUtils.loadImage("wall1.tsx");
                 this.renderer = new SingleImageRenderer(image);
                 break;
             }
 
-            case 1: {
-                isType = 1;
-                BufferedImage image = SpriteUtils.loadImage("wall_colum.tsx");
+            case 4: {
+                isType = 4;
+                BufferedImage image = SpriteUtils.loadImage("wall3.tsx");
                 this.renderer = new SingleImageRenderer(image);
                 break;
             }
 
-            case 1: {
-                isType = 1;
-                BufferedImage image = SpriteUtils.loadImage("wall_colum.tsx");
+            case 5: {
+                isType = 5;
+                BufferedImage image = SpriteUtils.loadImage("wall_hori.tsx");
                 this.renderer = new SingleImageRenderer(image);
                 break;
             }
 
-            case 1: {
-                isType = 1;
-                BufferedImage image = SpriteUtils.loadImage("wall_colum.tsx");
+            case 6: {
+                isType = 6;
+                BufferedImage image = SpriteUtils.loadImage("wall2.tsx");
                 this.renderer = new SingleImageRenderer(image);
                 break;
             }
 
-            case 1: {
-                isType = 1;
-                BufferedImage image = SpriteUtils.loadImage("wall_colum.tsx");
+            case 7: {
+                isType = 7;
+                BufferedImage image = SpriteUtils.loadImage("wall4.tsx");
                 this.renderer = new SingleImageRenderer(image);
                 break;
             }
 
-            case 1: {
-                isType = 1;
-                BufferedImage image = SpriteUtils.loadImage("wall_colum.tsx");
+            case 8: {
+                isType = 8;
+                BufferedImage image = SpriteUtils.loadImage("wall5.tsx");
                 this.renderer = new SingleImageRenderer(image);
                 break;
             }
 
-            case 1: {
-                isType = 1;
-                BufferedImage image = SpriteUtils.loadImage("wall_colum.tsx");
+            case 9: {
+                isType = 9;
+                BufferedImage image = SpriteUtils.loadImage("wall8.tsx");
                 this.renderer = new SingleImageRenderer(image);
                 break;
             }
 
-            case 1: {
-                isType = 1;
-                BufferedImage image = SpriteUtils.loadImage("wall_colum.tsx");
+            case 10: {
+                isType = 10;
+                BufferedImage image = SpriteUtils.loadImage("wall6.tsx");
+                this.renderer = new SingleImageRenderer(image);
+                break;
+            }
+
+            case 11: {
+                isType = 11;
+                BufferedImage image = SpriteUtils.loadImage("wall7.tsx");
+                this.renderer = new SingleImageRenderer(image);
+                break;
+            }
+
+            case 12: {
+                isType = 12;
+                BufferedImage image = SpriteUtils.loadImage("wall9.tsx");
+                this.renderer = new SingleImageRenderer(image);
+                break;
+            }
+
+            case 13: {
+                isType = 13;
+                BufferedImage image = SpriteUtils.loadImage("wall8.tsx");
                 this.renderer = new SingleImageRenderer(image);
                 break;
             }
